d8eaabe02cc693bbc0bea894aba3064a3c7dfcbf
autumn-ragland <raglanda998@gmail.com>
2019-01-15 16:43:53 -0600

    completed CW3

--- CW3.js
+++ CW3.js
@@ -1,32 +1,52 @@
-//use object literal notation to create an array
-//add code to that array
-
+//create array
 var peopleList =
     [
-    {firstname : "Jimmy", lastname: "Page" , age: "62" , zipCode : "00281"},
+    {firstname : "Jimmy", lastname: "Page" , age: "62" , zipCode : "10281"},
     {firstname : "Rick", lastname: "Neilson" , age: "57" , zipCode : "61016"},
     {firstname : "Jimi", lastname: "Hendrix" , age: "57" , zipCode : "90001"},
     {firstname : "Lemny", lastname: "Kilmeiser" , age: "57" , zipCode : "21120"}
     ];
+console.log(peopleList[0].firstname);
 
-peopleList[0].famousSong = "purple haze";
-peopleList[1].famousSong = "rock and roll";
-peopleList[2].famousSong = "summer time song";
-peopleList[3].famousSong = "loving and living";
+//add famous song to Hendrix
+peopleList[2].famousSong = "purple haze";
+console.log(peopleList[2].famousSong);
 
+//add function to return band and zip code
 peopleList[0].getBandAndZip = function()
 {
-    return "Led Zeppelin HISZIPCODE " + this.zipCode
+    return "Led Zeppelin " + this.zipCode
 };
 
+//zipcode match function
+var input = prompt("Enter a zip code");
 
+function zipPrint(input)
+{
+    if (input == 10281)
+    {
+      return peopleList[0].firstname
+    }
+    else if (input == 61016)
+    {
+        return peopleList[1].firstname
+    }
+    else if (input == 90001)
+    {
+        return peopleList[2].firstname
+    }
+    else if (input == 21120)
+    {
+        return peopleList[3].firstname
+    }
+    else
+    {
+        return "NO MATCH"
+    }
+}
 
-console.log(peopleList[0].firstname);
-console.log(peopleList[0].lastname);
-console.log(peopleList[0].age);
-console.log(peopleList[0].zipCode);
-console.log(peopleList[0].famousSong);
-console.log(peopleList[0].getBandAndZip());
+console.log(zipPrint(input));
+
 
 
 
